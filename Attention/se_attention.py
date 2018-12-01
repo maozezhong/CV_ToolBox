35ef2cd2cbd7b47ef2032d5a87af0b87394ec1d2
maozezhong <407490251@qq.com>
2018-12-01 22:45:23 +0800

    add se attetion

---
+++ Attention/se_attention.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+def squeeze_excitation_layer(x, out_dim, ratio=16):
+    '''
+        SE channel attention
+        input:
+            x
+            out_dim : channel default
+            ratio : reduction rate, defualt 16
+    '''
+    squeeze = layers.GlobalAveragePooling2D()(x)
+    excitation = layers.Dense(out_dim//ratio, activation='relu')(squeeze)
+    excitation = layers.Dense(out_dim, activation='sigmoid')
+    excitation = layers.Reshape((1, 1, out_dim))(excitation)
+
+    scale = layers.multiply([x, excitation])
+
+    return scale

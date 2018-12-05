c2e8b1a76130327a9ce4fe08d0f13609cc3d2c50
maozezhong <407490251@qq.com>
2018-12-05 19:37:19 +0800

    fix a bug

--- Attention/se_attention.py
+++ Attention/se_attention.py
@@ -10,7 +10,7 @@ def squeeze_excitation_layer(x, out_dim, ratio=16):
     '''
     squeeze = layers.GlobalAveragePooling2D()(x)
     excitation = layers.Dense(out_dim//ratio, activation='relu')(squeeze)
-    excitation = layers.Dense(out_dim, activation='sigmoid')
+    excitation = layers.Dense(out_dim, activation='sigmoid')(excitation)
     excitation = layers.Reshape((1, 1, out_dim))(excitation)
 
     scale = layers.multiply([x, excitation])
